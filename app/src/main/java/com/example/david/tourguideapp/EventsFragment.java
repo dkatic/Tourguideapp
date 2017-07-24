e3604e9caab26ada50a4e2575e617ae75279b5ff
David Katić <david.katic@gmail.com>
2017-07-25 00:43:59 +0200

    add image, and strings

--- app/src/main/java/com/example/david/tourguideapp/EventsFragment.java
+++ app/src/main/java/com/example/david/tourguideapp/EventsFragment.java
@@ -18,11 +18,11 @@ public class EventsFragment extends Fragment {
         View rootView =  inflater.inflate(R.layout.list_layout, container, false);
 
         ArrayList<Events> events = new ArrayList<>();
-        events.add(new Events(getString(R.string.events_name_1), getString(R.string.events_address_1), getString(R.string.events_opening_hours_1), getString(R.string.events_description_1), R.drawable.events_1));
-        events.add(new Events(getString(R.string.events_name_2), getString(R.string.events_address_2), getString(R.string.events_opening_hours_2), getString(R.string.events_description_2), R.drawable.events_2));
-        events.add(new Events(getString(R.string.events_name_3), getString(R.string.events_address_3), getString(R.string.events_opening_hours_3), getString(R.string.events_description_3), R.drawable.events_3));
-        events.add(new Events(getString(R.string.events_name_4), getString(R.string.events_address_4), getString(R.string.events_opening_hours_4), getString(R.string.events_description_4), R.drawable.events_4));
-        events.add(new Events(getString(R.string.events_name_5), getString(R.string.events_address_5), getString(R.string.events_opening_hours_5), getString(R.string.events_description_5), R.drawable.events_5));
+        events.add(new Events(getString(R.string.events_name_1), getString(R.string.events_address_1), getString(R.string.events_date_1), getString(R.string.events_description_1), R.drawable.events_1));
+        events.add(new Events(getString(R.string.events_name_2), getString(R.string.events_address_2), getString(R.string.events_date_2), getString(R.string.events_description_2), R.drawable.events_2));
+        events.add(new Events(getString(R.string.events_name_3), getString(R.string.events_address_3), getString(R.string.events_date_3), getString(R.string.events_description_3), R.drawable.events_3));
+        events.add(new Events(getString(R.string.events_name_4), getString(R.string.events_address_4), getString(R.string.events_date_4), getString(R.string.events_description_4), R.drawable.events_4));
+        events.add(new Events(getString(R.string.events_name_5), getString(R.string.events_address_5), getString(R.string.events_date_5), getString(R.string.events_description_5), R.drawable.events_5));
 
         EventsAdapter adapter = new EventsAdapter(getActivity(), events);
         ListView listView = (ListView) rootView.findViewById(R.id.list);
--- app/src/main/java/com/example/david/tourguideapp/Hotels.java
+++ app/src/main/java/com/example/david/tourguideapp/Hotels.java
@@ -4,7 +4,7 @@ package com.example.david.tourguideapp;
  * Created by david on 24.7.2017..
  */
 
-public class Hotels extends Events {
+public class Hotels extends EatDrink {
 
     public Hotels(String name, String adress, String openingHours, String description, int imageResourceId) {
         super(name, adress, openingHours, description, imageResourceId);
